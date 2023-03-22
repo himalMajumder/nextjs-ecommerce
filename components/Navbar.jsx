1cd88e3175ab87c62f6fcedece4a3c68d728500b
himalMajumder <himalmajumder@gmail.com>
2023-03-22 14:53:23 +0600

    Creating cart state

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,8 +6,8 @@ import { BsFillBagCheckFill } from 'react-icons/bs'
 
 // AiOutlineShoppingCart
 
-const Navbar = () => {
-
+const Navbar = ({ cart, addToCart,clearCart, subTotal, removeFromCart}) => {
+ 
   const toggleCart = () => {
 
     if (ref.current.classList.contains('translate-x-full')) {
@@ -44,35 +44,23 @@ const Navbar = () => {
         <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
         <span onClick={toggleCart} className='absolute top-2 right-2 cursor-pointer text-2xl text-pink-500' ><AiFillCloseCircle /></span>
         <ol className='list-decimal font-semibold'>
-          <li>
-            <div className="item flex my-5">
-              <div className='w-2/3 font-semibold'>Tshirt - Wear the code safas asfasf a ads fasf as asf asdf asf asdf</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold text-lg '>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500' /> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className='cursor-pointer text-pink-500' />
+          { Object.keys(cart).length === 0 && <div className='my-4 font-semibold'>Your cart is Empty!</div>}
+          { Object.keys(cart).map((k) => { 
+            return (  <li key={k}>
+              <div className="item flex my-5">
+                <div className='w-2/3 font-semibold'>{ cart[k].name }</div>
+                <div className='flex items-center justify-center w-1/3 font-semibold text-lg '>
+                  <AiFillMinusCircle onClick={(e) =>  removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)} 
+                  className='cursor-pointer text-pink-500' /> <span className="mx-2 text-sm">{ cart[k].qty }</span> <AiFillPlusCircle  onClick={(e) =>  addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)} className='cursor-pointer text-pink-500' />
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className="item flex my-5">
-              <div className='w-2/3 font-semibold'>Tshirt - Wear the code safas asfasf a ads fasf as asf asdf asf asdf</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold text-lg '>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500' /> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className='cursor-pointer text-pink-500' />
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item flex my-5">
-              <div className='w-2/3 font-semibold'>Tshirt - Wear the code safas asfasf a ads fasf as asf asdf asf asdf</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold text-lg '>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500' /> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className='cursor-pointer text-pink-500' />
-              </div>
-            </div>
-          </li>
-
+            </li>)
+          })}
+          
         </ol>
         <div className="flex">
           <button className="flex mr-2 mx-auto text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm"> <BsFillBagCheckFill className='m-1' /> Checkout </button>
-          <button className="flex mr-2 mx-auto text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">   Clear Cart </button>
+          <button onClick={clearCart} className="flex mr-2 mx-auto text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">Clear Cart</button>
         </div>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,91 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import "./../styles/globals.css";
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import './../styles/globals.css';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }) {
+	const [cart, setCart] = useState({});
+	const [subTotal, setSubTotal] = useState(0);
+
+	useEffect(() => { 
+		try {
+			if(localStorage.getItem('cart')){
+				setCart(JSON.parse(localStorage.getItem('cart')));
+			}
+		} catch (error) {
+			console.log(error);
+			localStorage.clear();
+		}
+	}, [])
+	
+
+	/**
+	 * Save Cart
+	 * @return void
+	 */
+	const saveCart = (myCart) => { 
+		localStorage.setItem('cart', JSON.stringify(myCart));
+		let subT = 0;
+		let keys = Object.keys(myCart)
+		for(let i =0; i < keys.length; i++){ 
+			subT += myCart[keys[i]].price * myCart[keys[i]].qty;
+		}
+		setSubTotal(subT);  
+	}
+
+	/**
+	 * Add To Cart	
+	 * @param {sting} itemCode 
+	 * @param {int} qty 
+	 * @param {float} price 
+	 * @param {string} name 
+	 * @param {string} size 
+	 * @param {string} variant 
+	 * @return void
+	 */
+	const addToCart = (itemCode, qty, price, name, size, variant) => {
+		let newCart = cart;
+		if(itemCode in cart){
+			newCart[itemCode].qty = cart[itemCode].qty + qty;
+		}
+		else{
+			newCart[itemCode] = { qty : 1, price, name, size, variant}
+		}
+		setCart(newCart);
+		saveCart(newCart);  
+	}
+
+
+	/**
+	 * Clear Cart
+	 * @return void
+	 */
+	const clearCart = () => {
+		setCart({});
+		saveCart({});
+	}
+
+	/**
+	 * Remove from Cart
+	 * @return void
+	 */
+	const removeFromCart = (itemCode, qty, price, name, size, variant) => { 
+		let newCart = JSON.parse(JSON.stringify(cart));
+		if(itemCode in cart){
+			newCart[itemCode].qty = cart[itemCode].qty - qty;
+		}
+		if(newCart[itemCode].qty <= 0){
+			delete newCart[itemCode];
+		}
+		setCart(newCart);
+		saveCart(newCart);
+	}
+
 	return (
 		<>
-			<Navbar />
-			<Component {...pageProps} /> 
-      <Footer />
+			<Navbar  cart={cart} addToCart={addToCart} clearCart={clearCart} subTotal={subTotal} removeFromCart={removeFromCart} />
+			<Component cart={cart} addToCart={addToCart} clearCart={clearCart} subTotal={subTotal} removeFromCart={removeFromCart}  {...pageProps} />
+			<Footer />
 		</>
 	);
 }
